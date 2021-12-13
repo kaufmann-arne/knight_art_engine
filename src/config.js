66e6612c6fc69f42099efc5c40eb5db9a24f1824
Mage Morgan <fortressdaofinance@gmail.com>
2021-12-13 12:53:07 +0100

    Add files via upload

--- src/config.js
+++ src/config.js
@@ -1,30 +1,40 @@
-const basePath = process.cwd();
-const { MODE } = require(`${basePath}/constants/blend_mode.js`);
-const { NETWORK } = require(`${basePath}/constants/network.js`);
+"use strict";
 
-const network = NETWORK.eth;
+const path = require("path");
+const isLocal = typeof process.pkg === "undefined";
+const basePath = isLocal ? process.cwd() : path.dirname(process.execPath);
+const { MODE } = require(path.join(basePath, "src/blendMode.js"));
+const description =
+  "Mighty Avax Knights protecting the Fort";
+const baseUri = "ipfs://QmRsWZFoGfmT2S58N4pDyYAT7yWMTKtDjyiqey6uv9Jvec";
 
-// General metadata for Ethereum
-const namePrefix = "Your Collection";
-const description = "Remember to replace this description";
-const baseUri = "ipfs://NewUriToReplace";
-
-const solanaMetadata = {
-  symbol: "YC",
-  seller_fee_basis_points: 1000, // Define how much % you want from secondary market sales 1000 = 10%
-  external_url: "https://www.youtube.com/c/hashlipsnft",
-  creators: [
-    {
-      address: "7fXNuer5sbZtaTEPhtJ5g5gNtuyRoKkvxdjEjEnPN4mC",
-      share: 100,
-    },
-  ],
-};
-
-// If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
   {
-    growEditionSizeTo: 5,
+    growEditionSizeTo: 6,
+    layersOrder: [
+      { name: "Background" },
+      { name: "Eyeball" },
+      { name: "Eye color" },
+      { name: "Iris" },
+      { name: "Shine" },
+      { name: "Bottom lid" },
+      { name: "Top lid" },
+    ],
+  },
+  {
+    growEditionSizeTo: 56,
+    layersOrder: [
+      { name: "Background" },
+      { name: "Eyeball" },
+      { name: "Eye color" },
+      { name: "Iris" },
+      { name: "Shine" },
+      { name: "Bottom lid" },
+      { name: "Top lid" },
+    ],
+  },
+  {
+    growEditionSizeTo: 256,
     layersOrder: [
       { name: "Background" },
       { name: "Eyeball" },
@@ -37,45 +47,18 @@ const layerConfigurations = [
   },
 ];
 
-const shuffleLayerConfigurations = false;
+const shuffleLayerConfigurations = true;
 
 const debugLogs = false;
 
 const format = {
   width: 512,
   height: 512,
-  smoothing: false,
-};
-
-const gif = {
-  export: false,
-  repeat: 0,
-  quality: 100,
-  delay: 500,
-};
-
-const text = {
-  only: false,
-  color: "#ffffff",
-  size: 20,
-  xGap: 40,
-  yGap: 40,
-  align: "left",
-  baseline: "top",
-  weight: "regular",
-  family: "Courier",
-  spacer: " => ",
-};
-
-const pixelFormat = {
-  ratio: 2 / 128,
 };
 
 const background = {
   generate: true,
   brightness: "80%",
-  static: false,
-  default: "#000000",
 };
 
 const extraMetadata = {};
@@ -87,17 +70,8 @@ const uniqueDnaTorrance = 10000;
 const preview = {
   thumbPerRow: 5,
   thumbWidth: 50,
-  imageRatio: format.height / format.width,
+  imageRatio: format.width / format.height,
   imageName: "preview.png",
-};
-
-const preview_gif = {
-  numberOfImages: 5,
-  order: "ASC", // ASC, DESC, MIXED
-  repeat: 0,
-  quality: 100,
-  delay: 500,
-  imageName: "preview.gif",
 };
 
 module.exports = {
@@ -112,11 +86,4 @@ module.exports = {
   shuffleLayerConfigurations,
   debugLogs,
   extraMetadata,
-  pixelFormat,
-  text,
-  namePrefix,
-  network,
-  solanaMetadata,
-  gif,
-  preview_gif,
 };
